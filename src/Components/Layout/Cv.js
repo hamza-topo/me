421468a803013f9cd6484e93f2181cd1c6c5dee9
Topo <hamzaaitsidisaid.11@gmail.com>
2024-09-05 11:25:10 +0200

    CV oath

--- src/Components/Layout/Cv.js
+++ src/Components/Layout/Cv.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 // /
 export function Cv() {
     useEffect(() => {
-        const pdfPath = '/CV_2024-02-12_Hamza_Ait sidi said_en.pdf';
+        const pdfPath = '/me/CV_2024-02-12_Hamza_Ait sidi said_en.pdf';
 
         const a = document.createElement('a');
         a.href = pdfPath;
